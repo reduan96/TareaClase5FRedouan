58ba3100af6e96de14f1e3bc306cfa15f9499011
reduan96 <redu1996@gmail.com>
2021-03-03 19:43:33 +0100

    Creacion del atributo matriz y de su constructor.

--- src/main/java/programa/CuadradoMagico.java
+++ src/main/java/programa/CuadradoMagico.java
@@ -5,10 +5,27 @@
  */
 package programa;
 
+import java.util.Random;
+
 /**
  *
  * @author redu2
  */
 public class CuadradoMagico {
-    
+
+    private int[][] matriz;
+    private static Random aleatorio = new Random();
+
+    public CuadradoMagico() {
+
+        matriz = new int[3][3];
+        
+        for (int i = 0; i < matriz.length; i++) {
+            
+            for (int j = 0; j < matriz[i].length; j++) {
+                
+                matriz[i][j] = aleatorio.nextInt((9-1)+1)+1;
+            }
+        }
+    }
 }
